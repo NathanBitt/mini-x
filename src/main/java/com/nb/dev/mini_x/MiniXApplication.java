fc98b5bc09feb337c6b35d805d61c075c4d4d42d
Nathan <nathanlb.dev@gmail.com>
2025-10-10 21:04:31 -0300

    base

---
+++ src/main/java/com/nb/dev/mini_x/MiniXApplication.java
@@ -0,0 +1,13 @@
+package com.nb.dev.mini_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nb/dev/mini_x/MiniXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nb.dev.mini_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
